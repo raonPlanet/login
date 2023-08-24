6c4bfc3a1ddd3157b3febe19be61f6fb3a5224a7
raonPlanet <do.it.raon@gmail.com>
2023-08-25 08:08:08 +0900

    study login 2nd commit

--- backend/api-server.js
+++ backend/api-server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 app.get('/api/account', (req, res) => {
     res.send({
@@ -8,7 +11,11 @@ app.get('/api/account', (req, res) => {
         memberName : "홍길동"
     })
 })
-
+app.post('/api/account', (req, res) => {
+    const loginId = req.body.loginId;
+    const loginPw = req.body.loginPw;
+    console.log(loginId + '/'+loginPw);
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,14 @@
     <div v-else>
       <label for="loginId">
         <span>아이디</span>
-        <input type="text" id="loginId"/>
+        <input type="text" id="loginId" v-model="state.form.loginId"/>
       </label>
       <label for="loginPw">
         <span>비밀번호</span>
-        <input type="password" id="loginPw"/>
+        <input type="password" id="loginPw" v-model="state.form.loginPw"/>
       </label>
       <hr/>
-      <button>로그인</button>
+      <button @click="submit()">로그인</button>
     </div>
   </div>
 </template>
@@ -26,13 +26,26 @@ export default {
       account: {
         mid: null,
         memberName: ""
+      },
+      form:{
+        loginId: "",
+        loginPw: ""
       }
     });
-    axios.get("/api/account").then((res) => {
-      console.log(res.data);
-      state.account = res.data;
-    });
-    return {state};
+    const submit = ()=>{
+      const args = {
+        loginId: state.form.loginId,
+        loginPw: state.form.loginPw
+      }
+      axios.post("/api/account",args).then((res)=>{
+        console.log(res);
+      })
+    }
+    // axios.get("/api/account").then((res) => {
+    //   console.log(res.data);
+    //   state.account = res.data;
+    // });
+    return {state,submit};
   }
 }
 
